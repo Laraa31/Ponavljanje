b3bccfb3654abcbb84e2d670bfc43031c512297c
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-02-01 12:21:55 +0100

    Add files via upload

---
+++ 2. zadatak/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prijestupne godine\n");
+            for(int godine=1900; godine < 2100; godine++)
+            {
+                if (godine % 400 == 0 || godine % 4 == 0 && godine%100!=0)
+                {
+                    Console.WriteLine(godine + "\n");
+                }
+            }
+            
+            
+            
+       
+            
+            Console.ReadKey();
+        }
+    }
+}
